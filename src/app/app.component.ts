329ffdce167c774fb4a97f0b6b3e097731014fe2
wcorderolara <wcordero.lara@gmail.com>
2019-08-09 10:05:44 -0600

    adding bootstrap, font-awesome and .gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DatingApp-SPA';
+  values: any = [];
+
+  constructor( private http: HttpClient) {}
+
+  ngOnInit(){
+    this.http.get('http://localhost:5000/api/values')
+        .subscribe( response => {
+          this.values = response;
+        });
+  }
+
 }
